09bcf5eeb0a8f39f3131430ec8933f1c1872584a
Hemang Vyas <vyashemang563@gmail.com>
2026-06-23 19:40:24 +0530

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.java
@@ -0,0 +1,34 @@
+class CountSquares {
+
+    HashMap<List<Integer>, Integer> ptsCount;
+    List<List<Integer>> pts;
+
+    public CountSquares() {
+        ptsCount = new HashMap<>();
+        pts = new ArrayList<>();
+    }
+    
+    public void add(int[] point) {
+        List<Integer> p = Arrays.asList(point[0], point[1]);
+        ptsCount.put(p, ptsCount.getOrDefault(p, 0) + 1);
+        pts.add(p);
+    }
+    
+    public int count(int[] point) {
+        int res = 0;
+        int px = point[0];
+        int py = point[1];
+
+        for (List<Integer> pt : pts) {
+            int x = pt.get(0);
+            int y = pt.get(1);
+
+            if (Math.abs(py - y) != Math.abs(px - x) || x == px || y == py) {
+                continue;
+            }
+            res += ptsCount.getOrDefault(Arrays.asList(x, py), 0) *
+                   ptsCount.getOrDefault(Arrays.asList(px, y), 0);
+        }
+        return res;
+    }
+}
